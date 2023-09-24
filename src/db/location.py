80990773ceb14e71ab9001c86dacb9085312db7a
Oskar Vihriälä <oskar.vihriala@gmail.com>
2023-09-24 23:52:05 +0300

    Add ability to create new sublocations

--- src/db/location.py
+++ src/db/location.py
@@ -2,72 +2,43 @@ from database import db
 from sqlalchemy import text
 
 
+# https://learnsql.com/blog/how-to-query-hierarchical-data/
 def get_location(path: list[str]):
     return db.session.execute(
         text(
             """
-            WITH RECURSIVE location_hierarchy AS (
-                SELECT id, parent_id, CAST(name AS text) AS path
-                FROM location
-                WHERE parent_id IS NULL
-                UNION ALL
-                SELECT location.id, location.parent_id, 
-                    CASE 
-                        WHEN location_hierarchy.path = '' 
-                        THEN location.name 
-                        ELSE location_hierarchy.path || '/' || location.name 
-                    END
-                FROM location, location_hierarchy
-                WHERE location.parent_id = location_hierarchy.id
-            )
-            SELECT * FROM location_hierarchy
-            WHERE path = :path;
+                WITH RECURSIVE location_hierarchy AS (
+                    SELECT id, parent_id, name, CAST(name AS text) AS path
+                    FROM location
+                    WHERE parent_id IS NULL
+                    UNION ALL
+                    SELECT location.id, location.parent_id, location.name, location_hierarchy.path || '/' || location.name 
+                    FROM location, location_hierarchy
+                    WHERE location.parent_id = location_hierarchy.id
+                )
+                SELECT id, name FROM location_hierarchy
+                WHERE path = :path;
             """
         ),
         {"path": "/".join(path)},
     ).fetchone()
 
 
-def get_sublocations(path: list[str]):
-    path_string = "/" + "/".join(path)
-    if path_string != "/":
-        path_string += "/"
-
-    return db.session.execute(
-        text(
-            """
-            WITH RECURSIVE location_hierarchy AS (
-                SELECT id, parent_id, name, '/' || name AS path
-                FROM location
-                WHERE parent_id IS NULL
-                UNION ALL
-                SELECT location.id, location.parent_id, location.name, location_hierarchy.path || '/' || location.name
-                FROM location, location_hierarchy
-                WHERE location.parent_id = location_hierarchy.id
-            )
-            SELECT * FROM location_hierarchy
-            WHERE path ~ ('^' || :path || '[^/]+$');
-            """
-        ),
-        {"path": path_string},
-    ).fetchall()
-
-
-def get_sublocations_by_id(location_id: int | None):
+def get_sublocations(location_id: int | None):
     if location_id is None:
         sql = text(
             """
-            SELECT id, parent_id, name
-            FROM location
-            WHERE parent_id IS NULL;
+                SELECT id, name
+                FROM location
+                WHERE parent_id IS NULL;
             """
         )
     else:
         sql = text(
             """
-            SELECT id, parent_id, name
-            FROM location
-            WHERE parent_id = :location_id;
+                SELECT id, name
+                FROM location
+                WHERE parent_id = :location_id;
             """
         )
 
@@ -77,22 +48,14 @@ def get_sublocations_by_id(location_id: int | None):
     ).fetchall()
 
 
-def get_parent_list(location_id):
-    result = db.session.execute(
+def create_location(location_name: str, parent_id: int | None):
+    db.session.execute(
         text(
             """
-            WITH RECURSIVE location_hierarchy AS (
-                SELECT id, parent_id      
-                FROM location
-                WHERE id = :location_id
-                UNION ALL
-                SELECT l.id, l.parent_id        
-                FROM location l
-                INNER JOIN location_hierarchy ph ON l.id = ph.parent_id
-            )
-            SELECT * FROM location_hierarchy;
+                INSERT INTO location (name, parent_id)
+                VALUES (:name, :parent_id);
             """
         ),
-        {"location_id": location_id},
+        {"name": location_name, "parent_id": parent_id},
     )
-    return list(map(lambda x: x, result.fetchall()))
+    db.session.commit()
--- src/routes/location.py
+++ src/routes/location.py
@@ -1,25 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
 from db.location import (
+    create_location,
     get_location,
-    get_parent_list,
     get_sublocations,
-    get_sublocations_by_id,
 )
 
 location_bp = Blueprint("location", __name__)
 
 
+def parse_path(raw_path: str) -> list[str]:
+    if raw_path == "":
+        return []
+    else:
+        return raw_path.split("/")
+
+
 @location_bp.route("/", defaults={"raw_path": ""})
 @location_bp.route("/<path:raw_path>/")
-def location(raw_path) -> str:
-    if raw_path == "":
-        path = []
-    else:
-        path = raw_path.split("/")
+def location(raw_path):
+    path = parse_path(raw_path)
 
     if len(path) == 0:
         location_info = None
-        child_locations = get_sublocations_by_id(None)
+        child_locations = get_sublocations(None)
     else:
         location_info = get_location(path)
         if location_info is None:
@@ -30,8 +33,28 @@ def location(raw_path) -> str:
                 ),
                 404,
             )
-        child_locations = get_sublocations_by_id(location_info.id)
+        child_locations = get_sublocations(location_info.id)
 
     return render_template(
-        "location.html", sublocations=child_locations, location_info=location_info
+        "location.html",
+        path=path,
+        sublocations=child_locations,
+        location_info=location_info,
     )
+
+
+@location_bp.route("/", defaults={"raw_path": ""}, methods=["POST"])
+@location_bp.route("/<path:raw_path>/", methods=["POST"])
+def new_location(raw_path):
+    path = parse_path(raw_path)
+
+    if len(path) == 0:
+        parent_id = None
+    else:
+        parent_id = get_location(path).id
+
+    location_name = request.form["name"]
+
+    create_location(location_name, parent_id)
+
+    return redirect(url_for("location.location", raw_path=raw_path))
